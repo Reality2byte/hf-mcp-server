2ee13a9c0da425b7b989687ad7dd5fcfed841661
evalstate <1936278+evalstate@users.noreply.github.com>
2025-11-10 18:50:11 +0000

    tweak import

--- packages/app/src/server/utils/gradio-result-processor.ts
+++ packages/app/src/server/utils/gradio-result-processor.ts
@@ -1,4 +1,4 @@
-import { CallToolResultSchema } from '@modelcontextprotocol/sdk/types.js';
+import type { CallToolResultSchema } from '@modelcontextprotocol/sdk/types.js';
 import { logger } from './logger.js';
 
 /**
